2be0c31f34a36e30b1608aa04a34ab956415b305
dhanish sudra <sudradhanish786@gmail.com>
2026-03-26 22:09:21 +0530

    Day 2: GetX routing setup + app_routes + app_pages + HomePage placeholder

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'app_routes.dart';
 import '../../presentation/pages/home_page.dart';
-
+import '../../presentation/pages/login_page.dart';
+import '../../presentation/binding/auth_binding.dart';
 class AppPages {
 
-  static final List<GetPage> pages = [
+  static final pages = [
     GetPage(name: AppRoutes.home, page: () => HomePage()),
+    GetPage(name: AppRoutes.login, page: () => LoginPage(), binding: AuthBinding()),
   ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:task_manager/app/routes/app_pages.dart';
-import 'package:task_manager/app/routes/app_routes.dart';
-void main() {
+import 'package:task_manager/app/routes/app_routes.dart'; // ← ye CLI ne generate kiya hai
+import 'firebase_options.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,15 +22,12 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Task Flow',
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.home,
+      initialRoute: AppRoutes.login,
       getPages: AppPages.pages,
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-
     );
   }
 }
-
-
